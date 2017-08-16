3d9fbbca57bbd95badba7cedfc1bb0d7f28482de
Dinh Duc <Lutin@Dinhs-MacBook-Air.local>
2017-08-16 12:57:01 +0300

    Add produces to application_json_value

--- src/main/java/mobile/api/iNails/controller/CategoryController.java
+++ src/main/java/mobile/api/iNails/controller/CategoryController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,7 +20,7 @@ public class CategoryController {
 	@Autowired
 	private CategoryDao categoryDao;
 	
-	@RequestMapping(value="categories", method= RequestMethod.GET)
+	@RequestMapping(value="categories", method= RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public ResponseEntity<List<Category>> findAllCategory(){
 		List<Category> categories = categoryDao.findAllCategories();
 		return (categories.isEmpty())? new ResponseEntity<List<Category>>(HttpStatus.NO_CONTENT) : new ResponseEntity<List<Category>>(categories, HttpStatus.OK);
--- src/main/java/mobile/api/iNails/controller/OrderController.java
+++ src/main/java/mobile/api/iNails/controller/OrderController.java
@@ -2,6 +2,7 @@ package mobile.api.iNails.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,7 @@ public class OrderController {
 	@Autowired
 	private OrderDao orderDao;
 	
-	@RequestMapping(value="invoices/{orderID}",method = RequestMethod.GET)
+	@RequestMapping(value="invoices/{orderID}",method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Invoice> findInvoiceByOrderNumber(@PathVariable("orderID") Integer orderID){
 		Invoice invoice = orderDao.findInvoiceByOrderNumber(orderID);
 		return (invoice == null)? new ResponseEntity<Invoice>(HttpStatus.NO_CONTENT) : new ResponseEntity<Invoice>(invoice,HttpStatus.OK);
