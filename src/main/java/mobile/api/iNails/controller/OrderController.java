0e195ab6dbbe0a2ca0af81fe049ce12e067aa356
Dinh Duc <Lutin@Dinhs-MacBook-Air.local>
2017-08-16 18:19:07 +0300

    Fix access-allow-control-origin

--- src/main/java/mobile/api/iNails/controller/OrderController.java
+++ src/main/java/mobile/api/iNails/controller/OrderController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,10 +19,12 @@ public class OrderController {
 	@Autowired
 	private OrderDao orderDao;
 	
+	@CrossOrigin
 	@RequestMapping(value="invoices/{orderID}",method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Invoice> findInvoiceByOrderNumber(@PathVariable("orderID") Integer orderID){
 		Invoice invoice = orderDao.findInvoiceByOrderNumber(orderID);
 		return (invoice == null)? new ResponseEntity<Invoice>(HttpStatus.NO_CONTENT) : new ResponseEntity<Invoice>(invoice,HttpStatus.OK);
+		
 	}
 
 }
