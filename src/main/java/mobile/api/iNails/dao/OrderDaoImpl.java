819a96d48989406ee5d53be784dbbabcf30cb052
Dinh Duc <Lutin@Dinhs-MacBook-Air.local>
2017-08-20 20:54:53 +0300

    Fix sql query

--- src/main/java/mobile/api/iNails/dao/OrderDaoImpl.java
+++ src/main/java/mobile/api/iNails/dao/OrderDaoImpl.java
@@ -29,7 +29,7 @@ public class OrderDaoImpl implements OrderDao {
 
 	@Override
 	public Order findOrder(int orderID) {
-		String sql = "SELECT * FROM Order WHERE OrderID = :orderID";
+		String sql = "SELECT * FROM orders WHERE OrderID = :orderID";
 		RowMapper<Order> rm = new OrderRowMapper();
 		MapSqlParameterSource paramMap = new MapSqlParameterSource();
 		paramMap.addValue("orderID", orderID);
@@ -45,7 +45,7 @@ public class OrderDaoImpl implements OrderDao {
 
 	@Override
 	public int createOrder(Order order) {
-		String sql = "INSERT INTO Orders(CustomerID_FK,StatusID_FK,Date,TimeID_FK) VALUES (:customerID,:statusID,:orderDate,:timeID)";
+		String sql = "INSERT INTO orders(CustomerID_FK,StatusID_FK,Date,TimeID_FK) VALUES (:customerID,:statusID,:orderDate,:timeID)";
 		MapSqlParameterSource paramMap = new MapSqlParameterSource();
 		paramMap.addValue("customerID", order.getCustomer().getCustomerID());
 		paramMap.addValue("statusID", order.getStatus().getStatusID());
@@ -76,7 +76,7 @@ public class OrderDaoImpl implements OrderDao {
 	@Override
 	public Invoice findInvoiceByOrderNumber(int orderID) {
 		String sql = "SELECT o.OrderID,o.Date, GROUP_CONCAT(s.Name) as Services,sum(s.Time) as Duration,sum(s.price) as TotalPayment,c.Name as Customer,c.Phone as CustomerPhone,st.Name as Status, t.Time as TimeReservation, t.Date as DateReservation, a.Name as Artist"
-				+" FROM Artist as a, Time as t,Orders as o"
+				+" FROM Artist as a, Time as t,orders as o"
 				+" INNER JOIN Customer as c ON c.CustomerID = o.CustomerID_FK"
 				+" INNER JOIN Cart as ca ON ca.OrderID_FK = o.OrderID"
 				+" INNER JOIN Status as st ON st.StatusID = o.StatusID_FK"
@@ -96,7 +96,7 @@ public class OrderDaoImpl implements OrderDao {
 
 	@Override
 	public int getLastInsert() {
-		String sql = "SELECT * FROM Orders ORDER BY orderID DESC LIMIT 1";
+		String sql = "SELECT * FROM orders ORDER BY orderID DESC LIMIT 1";
 		RowMapper<Order> rm = new OrderRowMapper();
 		return jdbcTemplate.query(sql, rm).get(0).getOrderID();
 	}
